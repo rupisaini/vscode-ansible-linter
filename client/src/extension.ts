3355404b7a21150cf58fcc9a0b0e4c39e00f61ec
Rupi Saini <onkarsaini@gmail.com>
2018-04-17 22:24:46 +0100

    language selector bug fix

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -26,7 +26,7 @@ export function activate(context: ExtensionContext) {
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{scheme: 'untitled', language: 'ansible'}, {scheme: 'file', language: 'yaml'}],
+		documentSelector: [{scheme: 'file', language: 'ansible'}, {scheme: 'file', language: 'yaml'}],
 		synchronize: {
 			// Synchronize the setting section 'languageServerExample' to the server
 			configurationSection: 'ansibleLinter',
